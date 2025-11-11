0a9d088f98f1c29b55031f2d1f199f95433474c1
jheerenmcs <jheeren@maryvillechristian.org>
2025-11-11 17:11:41 -0600

    Update FTCRobotController.java
    Ryan wrote the code for this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCRobotController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCRobotController.java
@@ -1,4 +1,67 @@
 package org.firstinspires.ftc.teamcode;
 
-public class FTCRobotController {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "Drive")
+public class FTCRobotController extends OpMode
+{
+    DcMotor Front_Left;
+    DcMotor Front_Right;
+    DcMotor Back_Left;
+    DcMotor Back_Right;
+
+    int time;
+
+    @Override
+    public void init()
+    {
+        Front_Left = hardwareMap.dcMotor.get("Front Left");
+        Front_Right = hardwareMap.dcMotor.get("Front Right");
+        Back_Left = hardwareMap.dcMotor.get("Back Left");
+        Back_Right = hardwareMap.dcMotor.get("Back Right");
+
+        telemetry.addData("Status", "Ready to run!");
+        telemetry.update();
+
+    }
+
+    @Override
+    public void loop()
+    {
+        Front_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Left.setDirection(DcMotorSimple.Direction.FORWARD);
+        Back_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Back_Left.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        Front_Right.setPower(gamepad1.right_stick_y);
+        Front_Left.setPower(gamepad1.left_stick_y);
+        Back_Right.setPower(gamepad1.right_stick_y);
+        Back_Left.setPower(gamepad1.left_stick_y);
+
+        if (gamepad1.right_trigger > 0)
+        {
+            Front_Right.setPower(0.5);
+            Front_Left.setPower(-0.5);
+            Back_Right.setPower(-0.5);
+            Back_Left.setPower(0.5);
+        }
+        else if (gamepad1.left_trigger > 0)
+        {
+            Front_Right.setPower(-0.5);
+            Front_Left.setPower(0.5);
+            Back_Right.setPower(0.5);
+            Back_Left.setPower(-0.5);
+        }
+
+        telemetry.addData("[Front Left Odometer]",
+                Front_Right.getCurrentPosition());
+        telemetry.addData("[Front Right Odometer]",
+                Back_Left.getCurrentPosition());
+        telemetry.addData("[Back Odometer]",
+                Front_Left.getCurrentPosition());
+        
+    }
 }
