29cf9f887dc0af15f22f06d9ad16e6c01a149b6a
Ryan Walker <ryanw1470@icloud.com>
2025-11-21 17:26:30 -0600

    Rename class and add motor direction setup
    Renamed the class from Auto_Baller to Ball_Collector_AUTO for consistency with the filename. Added motor direction configuration and telemetry data for motor power and encoder positions to aid debugging and setup.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ball_Collector_AUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ball_Collector_AUTO.java
@@ -4,10 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Disabled
 @Autonomous(name = "Ball Collector")
-public class Auto_Baller extends LinearOpMode
+public class Ball_Collector_AUTO extends LinearOpMode
 {
     DcMotor Front_Left;
     DcMotor Front_Right;
@@ -24,5 +25,29 @@ public class Auto_Baller extends LinearOpMode
         Front_Right = hardwareMap.dcMotor.get("Front Right");
         Back_Left = hardwareMap.dcMotor.get("Back Left");
         Back_Right = hardwareMap.dcMotor.get("Back Right");
+
+        Front_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Left.setDirection(DcMotorSimple.Direction.FORWARD);
+        Back_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Back_Left.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        telemetry.addData("[Front Right Motor Power]",
+                Front_Right.getPower());
+        telemetry.addData("[Front Left Motor Power]",
+                Front_Left.getPower());
+        telemetry.addData("[Back Left Motor Power]",
+                Back_Left.getPower());
+        telemetry.addData("[Back Right Motor Power]",
+                Back_Right.getPower());
+        telemetry.addData("[Dead Wheel Left]",
+                Front_Right.getCurrentPosition());
+        telemetry.addData("[Dead Wheel Right]",
+                Front_Left.getCurrentPosition());
+        telemetry.addData( "[Dead Wheel Middle]",
+                Back_Left.getCurrentPosition());
+        telemetry.update();
+
+        waitForStart();
+        
     }
 }
