309c64afc7a054c384aa02fcf32b435db5147f8f
justryanw7926 <ryanw1470@icloud.com>
2025-12-09 17:32:49 -0600

    added backup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Shooter.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@Autonomous(name = "Lighting Auto (WIP)")
+public class Auto_Shooter extends LinearOpMode
+{
+    DcMotor Front_Left;
+    DcMotor Front_Right;
+    DcMotor Back_Left;
+    DcMotor Back_Right;
+    DcMotor Thrower;
+    //Servo Door;
+
+    int saveposition;
+
+    @Override
+    public void runOpMode () throws InterruptedException
+    {
+        Front_Left = hardwareMap.dcMotor.get("Front Left");
+        Front_Right = hardwareMap.dcMotor.get("Front Right");
+        Back_Left = hardwareMap.dcMotor.get("Back Left");
+        Back_Right = hardwareMap.dcMotor.get("Back Right");
+        Thrower = hardwareMap.dcMotor.get("Thrower");
+       // Door = hardwareMap.servo.get("Door");
+
+        //Set motor directions
+        Front_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Left.setDirection(DcMotorSimple.Direction.FORWARD);
+        Back_Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        Back_Left.setDirection(DcMotorSimple.Direction.FORWARD);
+        Thrower.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //Telemetry start data
+        telemetry.addData("[Front Right Motor Power]",
+                Front_Right.getPower());
+        telemetry.addData("[Front Left Motor Power]",
+                Front_Left.getPower());
+        telemetry.addData("[Back Left Motor Power]",
+                Back_Left.getPower());
+        telemetry.addData("[Back Right Motor Power]",
+                Back_Right.getPower());
+        telemetry.addData("[Dead Wheel Left]",
+                Front_Right.getCurrentPosition());
+        telemetry.addData("[Dead Wheel Right]",
+                Front_Left.getCurrentPosition());
+        telemetry.addData( "[Dead Wheel Middle]",
+                Back_Left.getCurrentPosition());
+        telemetry.update();
+
+        waitForStart();
+
+        Front_Right.setPower(-0.5);
+        Front_Left.setPower(0);
+        Back_Right.setPower(-0.5);
+        Back_Left.setPower(0);
+
+        sleep(1000);
+    }
+}
