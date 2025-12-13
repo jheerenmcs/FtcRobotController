752707c815459960d9bc4e7fead74404c703263f
jheerenmcs <jheeren@maryvillechristian.org>
2025-12-12 18:01:53 -0600

    Added runtime

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ryan_Auto_5.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ryan_Auto_5.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @Autonomous(name = "Auto 5s Delay")
@@ -15,6 +16,8 @@ public class Ryan_Auto_5 extends LinearOpMode
     DcMotor Back_Right;
     DcMotor Thrower;
     //Servo Door;
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     int saveposition;
 
@@ -54,12 +57,17 @@ public class Ryan_Auto_5 extends LinearOpMode
 
         waitForStart();
 
-        wait(5);
 
         Front_Right.setPower(-0.5);
         Front_Left.setPower(-0.5);
         Back_Right.setPower(-0.5);
         Back_Left.setPower(-0.5);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.5))
+        {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
 
         sleep(1000);
     }
