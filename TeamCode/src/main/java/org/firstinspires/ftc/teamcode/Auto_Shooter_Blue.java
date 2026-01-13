fede27d009cc0ab492773bb005aff648b76584de
RYANWEATHER <ryanw1470@icloud.com>
2026-01-13 17:40:41 -0600

    working auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Shooter_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Shooter_Blue.java
@@ -128,6 +128,21 @@ public class Auto_Shooter_Blue extends LinearOpMode
         }
 
 
+        Front_Right.setPower(0);
+        Front_Left.setPower(0);
+        Back_Right.setPower(0);
+        Back_Left.setPower(0);
+        sleep(1000);
+
+        Front_Right.setPower(-0.5);
+        Front_Left.setPower(-0.5);
+        Back_Right.setPower(-0.5);
+        Back_Left.setPower(-0.5);
+        sleep(250);
+
+
+
+
 
     }
 }
